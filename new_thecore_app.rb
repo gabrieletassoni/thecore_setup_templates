86cb9f45a2cd1ad477ee3fa604dc83f856bdf7c5
Gabriele Tassoni <gabriele.tassoni@gmail.com>
2017-12-03 08:37:37 +0100

    A bit of labels

--- new_thecore_app.rb
+++ new_thecore_app.rb
@@ -4,7 +4,7 @@ current_gem_user = run "bundle config www.taris.it", capture: true
 # Set for the current user (/Users/iltasu/.bundle/config): "bah"
 credentials = current_gem_user.match(/^[\s\t]*Set for the current user .*: "(.*)"/)[1] rescue nil
 
-if credentials.blank? || yes? "Credentials already set, do you want to change them?", :red
+if credentials.blank? || yes?("Credentials already set, do you want to change them?", :red)
   username = ask "Please provide your username: ", :red
   password = ask "Please provide your password: ", :red
   credentials = "#{username}:#{password}"
@@ -18,10 +18,10 @@ add_source gems_repo do
   output = run "gem search ^thecore$ -ra --source #{gems_repo}", capture: true
   versions = output.match(/^[\s\t]*thecore \((.*)\)/)[1].split(", ") rescue []
   unless versions.empty?
-    answer = ask "Which version of thecore do you want to use?", :red, limited_to: versions.push("cancel")
-    answer = "1" if answer == "cancel"
+    answer = ask "Which version of thecore do you want to use?", :red, limited_to: versions
+    say "You selected #{answer}"
   end
-  gem 'thecore', "~> #{answer.split(".").first}" # , path: '../../thecore_project/thecore'
+  gem 'thecore', "~> #{answer.split(".").first(2).join(".")}" # , path: '../../thecore_project/thecore'
 
   all_gems_in_source = run "gem search -r --source #{gems_repo}", capture: true
 
@@ -36,11 +36,11 @@ add_source gems_repo do
   end
 
   useful_gems.each do |u_gem|
-    gem u_gem if u_gem.includes?("thecore_") && yes?("Would you like to use the gem '#{u_gem}' for this project?", :red)
+    gem u_gem if u_gem.include?("thecore_") && yes?("Would you like to use the gem '#{u_gem}' for this project?", :red)
   end
 
   useful_gems.each do |u_gem|
-    gem u_gem if u_gem.includes?("rails_admin_") && yes?("Would you like to use the gem '#{u_gem}' for this project?", :red)
+    gem u_gem if u_gem.include?("rails_admin_") && yes?("Would you like to use the gem '#{u_gem}' for this project?", :red)
   end
 end
 
