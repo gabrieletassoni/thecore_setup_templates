a1d824bc51fbc4dc5075ee018e7d164925c4a583
Gabriele Tassoni <gabriele.tassoni@gmail.com>
2018-09-03 23:40:59 +0200

    fix: remove unused templates, change template gitignore with the one generated by gitignore.io

--- new_thecore_app.rb
+++ new_thecore_app.rb
@@ -1,19 +1,3 @@
-# require 'net/dav'
-# Selecting the gems needed
-# current_gem_user = run "bundle config www.taris.it", capture: true
-# Set for the current user (/Users/iltasu/.bundle/config): "bah"
-# credentials = current_gem_user.match(/^[\s\t]*Set for the current user .*: "(.*)"/)[1] rescue nil
-
-# if credentials.blank? || yes?("Credentials already set, do you want to change them?", :red)
-#  username = ask "Please provide your username: ", :red
-#  password = ask "Please provide your password: ", :red
-#  credentials = "#{username}:#{password}"
-#  run "bundle config www.taris.it '#{credentials}'"
-#  run "gem sources --add 'https://#{credentials}@www.taris.it/gems-repo/'"
-# end
-
-# gems_repo = "https://www.taris.it/gems-repo/"
-
 # Asking which thecore gem to install
 output = run 'gem search ^thecore$ -ra', capture: true
 versions = (begin
@@ -21,10 +5,7 @@ versions = (begin
             rescue StandardError
               []
             end)
-# unless versions.empty?
-#  answer = ask "Which version of thecore do you want to use?", :red, limited_to: versions
-#  say "You selected #{answer}"
-# end
+
 version = "~> #{begin
                   versions.first.split('.').first(2).join('.')
                 rescue StandardError
@@ -56,45 +37,26 @@ theme = ask "Which theme for thecore do you want to use?", :red, limited_to: the
 say "You selected #{theme}"
 gem theme, themes[theme], require: theme
 
-# add_source gems_repo do
-#   output = run "gem search ^thecore$ -ra", capture: true
-#   versions = (output.match(/^[\s\t]*thecore \((.*)\)/)[1].split(", ") rescue [])
-#   unless versions.empty?
-#     answer = ask "Which version of thecore do you want to use?", :red, limited_to: versions
-#     say "You selected #{answer}"
-#   end
-#   gem 'thecore', "~> #{answer.split(".").first(2).join(".") rescue '1.0'}" # , path: '../../thecore_project/thecore'
-#
-#   # all_gems_in_source = run "gem search -r --source #{gems_repo}", capture: true
-#
-#   dav = Net::DAV.new(gems_repo, :curl => false)
-#   dav.verify_server = false
-#   dav.credentials(*(credentials.split(":")))
-#
-#   useful_gems = []
-#   dav.find('.',:recursive=>true,:suppress_errors=>true,:filename=>/\.gem$/) do | item |
-#     thegem = item.url.to_s[/(thecore_.*|rails_admin_.*)\-.*\.gem$/, 1]
-#     useful_gems.push(thegem) unless thegem.nil? || thegem.empty?
-#   end
-#
-#   useful_gems.uniq!
-#
-#   useful_gems.each do |u_gem|
-#     gem u_gem if u_gem.include?("thecore_") && yes?("Would you like to use the gem '#{u_gem}' for this project?", :red)
-#   end
-#
-#   useful_gems.each do |u_gem|
-#     gem u_gem if u_gem.include?("rails_admin_") && yes?("Would you like to use the gem '#{u_gem}' for this project?", :red)
-#   end
-# end
-
 # Run bundle
 run 'bundle'
 
 # then run thecorize_plugin generator
 rails_command "g thecore:thecorize_app #{@name}"
 
-# DB
-## Cannot use until I find a suitable way to deal with postgresql password for development
-# rails_command "db:create"
-# rails_command "db:migrate"
+      #remove the index.html
+      remove_file 'public/index.html'
+
+    # Add thecore references to js and css
+    # *= require thecore to application.css before */
+    # //= require thecore to application.js before //= require_tree .
+      inject_into_file 'app/assets/javascripts/application.js', before: "//= require_tree ." do <<-'RUBY'
+        puts "//= require thecore"
+        RUBY
+      end
+      inject_into_file 'app/assets/stylesheets/application.css', before: "*/ ." do <<-'RUBY'
+        puts "*= require thecore"
+        RUBY
+      end
+      
+    # TODO: remove from application controller the protect_from_forgery with: :exception part
+      gsub_file 'app/controllers/application_controller.rb', 'protect_from_forgery with: :exception', ''
