9588a846a62b2916a3815aea5d4355700e676e57
Gabriele Tassoni <gabriele.tassoni@gmail.com>
2019-02-28 13:05:47 +0100

    replace s. with spec.

--- new_thecore_component.rb
+++ new_thecore_component.rb
@@ -1,11 +1,11 @@
 # Edit GEMSPEC
 # Remove all dependencies created from the standard rails new command
-gsub_file "#{name}.gemspec", 's.add_dependency', '# s.add_dependency'
-gsub_file "#{name}.gemspec", 's.add_development_dependency', '# s.add_development_dependency'
+gsub_file "#{name}.gemspec", 'spec.add_dependency', '# spec.add_dependency'
+gsub_file "#{name}.gemspec", 'spec.add_development_dependency', '# spec.add_development_dependency'
 
-gsub_file "#{name}.gemspec", 's.homepage', "s.homepage = 'https://github.com/gabrieletassoni/thecore' #"
-gsub_file "#{name}.gemspec", 's.summary', "s.summary = 'Thecorized #{name}' #"
-gsub_file "#{name}.gemspec", 's.description', "s.description = 'Thecorized #{name} full description.' #"
+gsub_file "#{name}.gemspec", 'spec.homepage', "spec.homepage = 'https://github.com/gabrieletassoni/thecore' #"
+gsub_file "#{name}.gemspec", 'spec.summary', "spec.summary = 'Thecorized #{name}' #"
+gsub_file "#{name}.gemspec", 'spec.description', "spec.description = 'Thecorized #{name} full description.' #"
 
 # Getting higher version of thecore
 output = run 'gem search ^thecore$ -ra', capture: true
@@ -21,7 +21,7 @@ version = "~> #{begin
                   '1.0'
                 end}"
 inject_into_file "#{name}.gemspec", before: /^end/ do
-"    s.add_dependency 'thecore', '#{version}'\n"
+"  spec.add_dependency 'thecore', '#{version}'\n"
 end
 
 inject_into_file "lib/#{name}.rb", before: /^module #{Thor::Util.camel_case(name)}$/ do
