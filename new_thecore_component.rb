0f9ee7ffb1981f3e4fb448d053e6e08cb516bc02
Gabriele Tassoni <gabriele.tassoni@gmail.com>
2019-06-11 08:43:46 +0200

    Update new_thecore_component.rb

--- new_thecore_component.rb
+++ new_thecore_component.rb
@@ -3,9 +3,9 @@
 gsub_file "#{name}.gemspec", 'spec.add_dependency', '# spec.add_dependency'
 gsub_file "#{name}.gemspec", 'spec.add_development_dependency', '# spec.add_development_dependency'
 
-homepage = ask("Please provide url for this component's project page (i.e. https://github.com/gabrieletassoni/#{name}):\n")
-summary = ask("Please provide a short description for this component:\n")
-description = ask("Please provide a longer description for this component:\n")
+homepage = ask("Please provide url for this component's project page (i.e. https://github.com/gabrieletassoni/#{name}):\n", :green)
+summary = ask("Please provide a short description for this component:\n", :green)
+description = ask("Please provide a longer description for this component:\n", :green)
 
 gsub_file "#{name}.gemspec", 'spec.homepage', "spec.homepage = '#{homepage.presence || "https://github.com/gabrieletassoni/thecore"}' #"
 gsub_file "#{name}.gemspec", 'spec.summary', "spec.summary = '#{summary.presence || "Thecorized #{name}"}' #"
